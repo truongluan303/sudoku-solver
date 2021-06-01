f74384767ffdf90f03e143aa5e670fc3a12a1e76
Hoang Phuc Luan Truong <83048295+truongluan303@users.noreply.github.com>
2021-06-01 16:18:35 -0700

    Update SudokuFrame.java

--- SudokuFrame.java
+++ SudokuFrame.java
@@ -1,18 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
 
 
 /**
- *
  * @author Luan Truong
  */
+
+// This will be out main GUI
 public class SudokuFrame extends javax.swing.JFrame {
 
     private final int SIZE = 9;
@@ -40,7 +35,7 @@ public class SudokuFrame extends javax.swing.JFrame {
 
 
     /**
-     * This method is called from within the constructor to initialize the form.
+     * This method is called from within the constructor to initialize the GUI.
      */
     private void initComponents() {
 
@@ -128,8 +123,10 @@ public class SudokuFrame extends javax.swing.JFrame {
 
                 // if the input sudoku is solved successfully
                 if (results != null) {
+                    // Change the message box to inform the sudoku is solved
                     message.setBackground(new Color(142, 251, 123));
                     message.setText("The Result is:");
+                    // Display the solved sudoku
                     for (int i = 0; i < SIZE; i++)
                         for (int j = 0; j < SIZE; j++) {
                             inputs[i][j].setText(Integer.toString(results[i][j]));
@@ -138,6 +135,7 @@ public class SudokuFrame extends javax.swing.JFrame {
                 }
                 // if the input sudoku is unsolvable
                 else {
+                    // Change the message box to inform that the sudoku is unsolvable
                     message.setBackground(Color.RED);
                     message.setText("The given Sudoku is Unsolvable!");
                 }
@@ -149,12 +147,16 @@ public class SudokuFrame extends javax.swing.JFrame {
             /* If the input sudoku is solved and result is displayed, then
              * clicking the button will clear out the cells and get ready for new inputs */
             else {
-                for (int i = 0; i < SIZE; i++)
+                // Reset the input boxes to their initial state
+                for (int i = 0; i < SIZE; i++) {
                     for (int j = 0; j < SIZE; j++) {
                         inputs[i][j].setText("");
                         inputs[i][j].setEditable(true);
+                        inputs[i][j].setOpaque(false);
                     }
+                }
                 solved = false;
+                // Reset the message box and the button
                 okButton.setText("Solve");
                 message.setBackground(Color.WHITE);
                 message.setText("Please input the existing numbers in the sudoku below");
@@ -166,17 +168,21 @@ public class SudokuFrame extends javax.swing.JFrame {
 
 
     /**
-    * Turn the input in to a 9x9 array of integer
+    * Get the inputs from the text boxes and turn them into a 9x9 array of integer
      */
     private int[][] returnInput() {
         int[][] numbers = new int[SIZE][SIZE];
 
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
-                if (inputs[i][j].getText() == null || inputs[i][j].getText().equals(""))
+                if (inputs[i][j].getText() == null || inputs[i][j].getText().equals("")) {
                     numbers[i][j] = 0;
-                else
+                }
+                else {
                     numbers[i][j] = Integer.parseInt(inputs[i][j].getText());
+                    inputs[i][j].setOpaque(true);
+                    inputs[i][j].setBackground(Color.LIGHT_GRAY);
+                }
             }
         }
         return numbers;
