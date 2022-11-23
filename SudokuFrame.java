64608d7dd9d3dc1d638af88299ce2adf0a7dda4d
Luan Truong <83048295+truongluan303@users.noreply.github.com>
2022-11-22 16:51:11 -0800

    Update styling

--- SudokuFrame.java
+++ SudokuFrame.java
@@ -3,13 +3,7 @@ import java.awt.*;
 import java.awt.event.ActionListener;
 
 
-/**
- * @author Luan Truong
- */
-
-// This will be out main GUI
 public class SudokuFrame extends javax.swing.JFrame {
-
     private final int SIZE = 9;
     private JTextField message;
     private JTextField[][] inputs;
@@ -17,13 +11,8 @@ public class SudokuFrame extends javax.swing.JFrame {
     private JButton okButton;
     private boolean solved = false;
     private Printer myPrinter;
-    // End of variables declaration
 
-
-
-    /**
-     * Creates new form SudokuFrame
-     */
+    
     public SudokuFrame() {
         initComponents();
         this.setResizable(false);
@@ -33,13 +22,7 @@ public class SudokuFrame extends javax.swing.JFrame {
     }
 
 
-
-    /**
-     * This method is called from within the constructor to initialize the GUI.
-     */
     private void initComponents() {
-
-        // Variables declaration
         JPanel panel = new JPanel();
         JPanel[][] subPanels = new JPanel[3][3];
         message = new javax.swing.JTextField();
@@ -63,32 +46,35 @@ public class SudokuFrame extends javax.swing.JFrame {
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
-                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addComponent(panel, javax.swing.GroupLayout.DEFAULT_SIZE, 650, Short.MAX_VALUE)
-                        .addComponent(message, javax.swing.GroupLayout.DEFAULT_SIZE, 650, Short.MAX_VALUE)
-                        .addGroup(layout.createSequentialGroup()
-                                .addGap(260, 260, 260)
-                                .addComponent(okButton, javax.swing.GroupLayout.PREFERRED_SIZE, 131, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            layout
+            .createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(panel, javax.swing.GroupLayout.DEFAULT_SIZE, 650, Short.MAX_VALUE)
+            .addComponent(message, javax.swing.GroupLayout.DEFAULT_SIZE, 650, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+            .addGap(260, 260, 260)
+            .addComponent(okButton, javax.swing.GroupLayout.PREFERRED_SIZE, 131, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
-                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                                .addComponent(message, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(panel, javax.swing.GroupLayout.PREFERRED_SIZE, 650, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(okButton, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE))
+            layout
+            .createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+            .addComponent(message, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+            .addComponent(panel, javax.swing.GroupLayout.PREFERRED_SIZE, 650, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+            .addComponent(okButton, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
+        
         int count = 0;
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 count++;
                 subPanels[i][j] = new JPanel();
-                if (count % 2 == 0)
-                    subPanels[i][j].setBackground(new Color(179, 223, 244));
-                else
-                    subPanels[i][j].setBackground(new Color(252, 236, 182));
+                
+                if (count % 2 == 0) subPanels[i][j].setBackground(new Color(179, 223, 244));
+                else subPanels[i][j].setBackground(new Color(252, 236, 182));
+                
                 subPanels[i][j].setLayout(new java.awt.GridLayout(3, 3, 0, 0));
                 subPanels[i][j].setBorder(BorderFactory.createLineBorder(Color.black));
                 panel.add(subPanels[i][j]);
@@ -107,47 +93,43 @@ public class SudokuFrame extends javax.swing.JFrame {
     }
 
 
-
-
     /**
     * Actions performed when the button is clicked
      */
     private ActionListener action() {
         return e -> {
-            /* If the sudoku input is not solved yet, then clicking the button
-             * will solve the sudoku and return the result to the GUI */
+            // If the sudoku input is not solved yet, then clicking the button
+            // will solve the sudoku and return the result to the GUI
             if (!solved) {
                 int[][] inputArr = returnInput();
                 Solver mySolver = new Solver(returnInput());
                 results = mySolver.returnResult();
 
-                // if the input sudoku is solved successfully
+                // If the input sudoku is solved successfully,
+                // change the message box to inform the sudoku is solved and display the solved sudoku
                 if (results != null) {
-                    // Change the message box to inform the sudoku is solved
                     message.setBackground(new Color(142, 251, 123));
                     message.setText("The Result is:");
-                    // Display the solved sudoku
-                    for (int i = 0; i < SIZE; i++)
+                    for (int i = 0; i < SIZE; i++) {
                         for (int j = 0; j < SIZE; j++) {
                             inputs[i][j].setText(Integer.toString(results[i][j]));
                             inputs[i][j].setEditable(false);
                         }
+                    }
                 }
-                // if the input sudoku is unsolvable
+                // If the input sudoku is unsolvable,
+                // change the message box to inform that the sudoku is unsolvable
                 else {
-                    // Change the message box to inform that the sudoku is unsolvable
                     message.setBackground(Color.RED);
                     message.setText("The given Sudoku is Unsolvable!");
                 }
                 okButton.setText("OK");
                 solved = true;
-                // Output to the history text file
-                myPrinter.write(inputArr, results);
+                myPrinter.write(inputArr, results);  // Output to the history text file
             }
-            /* If the input sudoku is solved and result is displayed, then
-             * clicking the button will clear out the cells and get ready for new inputs */
+            // Once the input sudoku is solved and result is displayed,
+            // clicking the button will clear out the cells
             else {
-                // Reset the input boxes to their initial state
                 for (int i = 0; i < SIZE; i++) {
                     for (int j = 0; j < SIZE; j++) {
                         inputs[i][j].setText("");
@@ -163,8 +145,6 @@ public class SudokuFrame extends javax.swing.JFrame {
             }
         };
     }
-
-
 
 
     /**
